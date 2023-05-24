d492a5ef2c900ceeefbaa56e72b28a57dae5dc38
Erick Bryan Cubas <thecubas@outlook.com>
2023-05-23 22:45:50 -0300

    commit

---
+++ 1. Fundamentos de Python/tipos_de_dados.py
@@ -0,0 +1,10 @@
+print(11 + 10 + 1000) # Número Inteiro
+print(1.5 + 1 + 0.5) # Número Flutuante
+print(True) # Booleano -> Subclasse dos inteiros
+print(False) # Booleano -> Subclasse dos inteiros
+print("Python") # String
+
+int()
+float()
+str()
+bool()
