4beada511cea3026e9084d0c9a1df1a57cd0083d
Erick Bryan Cubas <thecubas@outlook.com>
2023-05-24 02:43:13 -0300

    commit

---
+++ 1. Fundamentos de Python/variaveis_e_constantes.py
@@ -0,0 +1,11 @@
+# Em Python não existe tecnicamente constantes
+
+# Variável
+age = 28
+name = 'Guilherme'
+print(f'Meu nome é {name} e eu tenho {age} anos(s) de idade.')
+
+# Modificando a variável
+age = 27
+name = 'Giovanna'
+print(f'Meu nome é {name} e eu tenho {age} anos(s) de idade.')
